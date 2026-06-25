d48ff34dc69b6998ad415340f9cc1db169205a4f
Kebert <rasmus.kebert@gmail.com>
2026-06-25 15:36:39 +0200

    Changed the dns to a .

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -34,7 +34,7 @@ resource "cloudflare_dns_record" "auth" {
 resource "cloudflare_dns_record" "dev_valhall" {
     zone_id = data.cloudflare_zone.main.id
 
-    name = "dev_valhall"
+    name = "dev.valhall"
     type = "CNAME"
 
     content = "${cloudflare_zero_trust_tunnel_cloudflared.server.id}.cfargotunnel.com"
